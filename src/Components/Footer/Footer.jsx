f3ffdfe94dfd0f660819770f6d73029a8fd65c59
naveensimha89 <naveensimha89@gmail.com>
2024-05-31 04:24:01 +0000

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-3" id="footer">
+      <p className="my-2 pt-3 align-items-center">
+        Copyright &copy; 2024 by naveen_kumar | All Rights Reserved
+        <br/>
+        contact no: 8978989066
+        <br/>
+        Mail: naveensimha89@gmail.com
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
